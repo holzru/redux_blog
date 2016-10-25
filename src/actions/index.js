efc17bef08674b07063790f08f73b6262ba46008
holzru <holzru@gmail.com>
2016-10-25 16:59:35 -0700

    errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export const FETCH_POSTS = "FETCH_POSTS";
 export const CREATE_POST = "CREATE_POST";
 
-const ROOT_URL = "http://reduxblog.heroku.com/api";
-const API_KEY = '?key=crazymamajama123';
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = '?key=12344423423crazymamajama123';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
