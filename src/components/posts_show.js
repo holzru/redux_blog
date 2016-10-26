d296bbad49493488d2b2e427f96237f22fe6dc37
holzru <holzru@gmail.com>
2016-10-25 21:28:29 -0700

    errors

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -3,14 +3,17 @@ import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
 
 class PostsShow extends Component {
-  componentWillMount(props) {
+  componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
   render() {
     return <div>Show post {this.props.params.id}</div>;
   }
+}
 
+function mapStateToProps(state) {
+  return { post: state.posts.post };
 }
 
 
-export default connect(null, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
